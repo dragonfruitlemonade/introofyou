96e1884ee0a5058141c0a72268aba8ddd612c9b5
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-28 08:58:34 +0900

    feat(components): 출생년도 및 이메일 자리 추가

--- pages/intro.js
+++ pages/intro.js
@@ -1,26 +1,45 @@
 import React from "react";
 import { useCallback } from "react";
-import { Form, Input, Button, Checkbox, Select } from "antd";
+import { Form, Input, Button, Checkbox, Select, Row, Col, DatePicker } from "antd";
 
 const { Option } = Select;
 import IndexLayout from "../components/IndexLayout";
 
 
 const Intro = () => {
+  const onChange = (date, dateString) => {
+    console.log(dateString);
+  };
+
   return (
     <IndexLayout>
       <Form style={{ padding: 10 }}>
-        <div>
-          <label htmlFor="user-sex">성별</label>
-          <br />
-          <Input.Group>
-            <Select defaultValue="선택하기">
-              <Option>남</Option>
-              <Option>여</Option>
-            </Select>
-          </Input.Group>
-        </div>
-       
+        <Row>
+          <Col span={11}>
+            <div>
+              <label htmlFor="user-sex">성별</label>
+              <hr />
+              <Input.Group>
+                <Select defaultValue="선택하기">
+                  <Option>남</Option>
+                  <Option>여</Option>
+                </Select>
+              </Input.Group>
+            </div>
+          </Col>
+          <Col span={2}></Col>
+          <Col span={11}>
+            출생년도
+            <hr />
+            <DatePicker onChange={onChange} />
+          </Col>
+          <Col span={24}><br/></Col>
+          <Col span={24}>
+            이메일
+            <hr />
+            tpghks9245@gmail.com
+          </Col>
+        </Row>
       </Form>
     </IndexLayout>
   );
