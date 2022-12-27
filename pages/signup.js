3a5fe7dd95dda4e4c69556837bbf6112d5734e0c
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-27 17:29:19 +0900

    feat(pages): signup.js 회원가입 창 아이디, 비밀번호, 비밀번호체크, 이메일형식지키기 기능 추가

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,77 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import { Form, Input, Checkbox, Button } from "antd";
+
+import IndexLayout from "../components/IndexLayout";
+import useInput from "../hooks/useInput";
 
 const Signup = () => {
+
+  const [email, onChangeEmail] = useInput("");
+  const [password, onChangePassword] = useInput("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordError(e.target.value !== password);
+      setPasswordCheck(e.target.value);
+    },
+    [password]
+  );
+
+  const onSubmit = useCallback(() => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+
+  }, [email, password, passwordCheck]);
+
   return (
-    <div>회원가입 페이지</div>
+    <IndexLayout>
+      <Form onFinish={onSubmit} style={{ padding: 10 }}>
+        <div>
+          <label htmlFor="user-email">아이디</label>
+          <br />
+          <Input
+            name="user-email"
+            type="email"
+            value={email}
+            required
+            onChange={onChangeEmail}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">비밀번호</label>
+          <br />
+          <Input
+            name="user-password"
+            type="password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password-check">비밀번호체크</label>
+          <br />
+          <Input
+            name="user-password-check"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && (
+            <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+          )}
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <Button type="primary" htmlType="submit">
+            가입하기
+          </Button>
+        </div>
+      </Form>
+    </IndexLayout>
   );
 };
 
