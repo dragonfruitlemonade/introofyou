5044fea17c181afa7a3e8a21690f0f3166327bdc
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-27 22:11:35 +0900

    chore: 띄워쓰기 고침

---
+++ components/PostForm.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+
+import useInput from "../hooks/useInput";
+
+const PostForm = () => {
+  const [text, onChangeText, setText] = useInput("");
+
+  const [term, setTerm] = useState("");
+  const [termError, setTermError] = useState(false);
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(false);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (!text || !text.trim()) {
+      return alert("게시글을 작성하세요.");
+    }
+
+    if (!term) {
+      return alert("자기소개서를 첨부해주세요.");
+    }
+  }, [text, term]);
+
+  return (
+    <Form
+      style={{ margin: "10px 0 20px" }}
+      encType="multipart/form-data"
+      onFinish={onSubmit}
+    >
+      <Input.TextArea
+        value={text}
+        onChange={onChangeText}
+        maxLength={140}
+        placeholder="자기소개서에 대한 내용"
+      />
+      <div>
+        <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+          자기소개서를 첨부합니다<span style={{ color: "red" }}>(필수)</span>
+        </Checkbox>
+        <Button type="primary" style={{ float: "right" }} htmlType="submit">
+          작성
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+export default PostForm;
