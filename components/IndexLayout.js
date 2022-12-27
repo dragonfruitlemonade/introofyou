45265aad2dad6f644cc1ac1a3ef38e5778a4fc57
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-27 12:59:36 +0900

    feat(IndexLayout): 기본틀 작성

--- components/IndexLayout.js
+++ components/IndexLayout.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import { Layout } from 'antd';
+import Link from 'next/link';
+import { Col, Row, Menu, Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
 
 const IndexLayout = ({ children }) => {
   return (
-    <Layout>
-      {children}
-    </Layout>
+    <Menu mode="horizontal" style={{ alignItems: "center" }}>
+      <Menu.Item>
+        <Link href="/">인트로오브유</Link>
+      </Menu.Item>
+      <Menu.Item key="intro">
+        <Link href="/intro">자기소개서</Link>
+      </Menu.Item>
+      <Menu.Item key="community">
+        <Link href="/community">커뮤니티</Link>
+      </Menu.Item>
+      <Menu.Item key="login">
+        <Link href="/login">로그인</Link>
+      </Menu.Item>
+      <Menu.Item key="signup">
+        <Link href="/signup">회원가입</Link>
+      </Menu.Item>
+    </Menu>
   );
 };
 
