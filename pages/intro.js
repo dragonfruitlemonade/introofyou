8fe8020ae997c2aa77438c399676745a051e81d2
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-27 17:40:31 +0900

    feat: 성별 고르는 버튼 추가

--- pages/intro.js
+++ pages/intro.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { useCallback } from "react";
+import { Form, Input, Button, Checkbox, Select } from "antd";
 
+const { Option } = Select;
 import IndexLayout from "../components/IndexLayout";
+
 
 const Intro = () => {
   return (
     <IndexLayout>
-      자소서 작성하는 페이지
+      <Form style={{ padding: 10 }}>
+        <div>
+          <label htmlFor="user-sex">성별</label>
+          <br />
+          <Input.Group>
+            <Select defaultValue="선택하기">
+              <Option>남</Option>
+              <Option>여</Option>
+            </Select>
+          </Input.Group>
+        </div>
+       
+      </Form>
     </IndexLayout>
   );
 };
