f832708b1901ca7aa8b2176920ae9b2e0fb5d9cd
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-28 16:36:47 +0900

    feat(sagas/index.js): 포스트사가 추가

--- sagas/index.js
+++ sagas/index.js
@@ -2,9 +2,13 @@ import { all, fork } from "redux-saga/effects";
 import axios from "axios";
 
 import userSaga from "./user";
+import postSaga from "./post";
 
 axios.defaults.withCredentials = true; // 쿠키 공유하기
 
 export default function* rootSaga() {
-  yield all([fork(userSaga)]);
+  yield all([
+    fork(postSaga),
+    fork(userSaga),
+  ]);
 }
