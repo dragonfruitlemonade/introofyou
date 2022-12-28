0ef53833c52c77f36c0d7999de86fcde7e925a1e
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-28 17:00:07 +0900

    feat: 무한 스크롤, 리듀서 추가

--- pages/community.js
+++ pages/community.js
@@ -1,15 +1,51 @@
-import React from "react";
-import IndexLayout from "../components/IndexLayout";
+import React from 'react';
+import { useEffect } from 'react';
+import IndexLayout from '../components/IndexLayout';
+import { useDispatch, useSelector } from 'react-redux';
 
-import PostCard from "../components/PostCard";
-import PostForm from "../components/PostForm";
+import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
+import { LOAD_POSTS_REQUEST } from '../reducers/post';
 
 const Community = () => {
+  const dispatch = useDispatch();
+  const { me } = useSelector((state) => state.user);
+  const { mainPosts, hasMorePost, loadPostsLoading } = useSelector(
+    (state) => state.post
+  );
+  
+  useEffect(() => {
+    dispatch({
+      type: LOAD_POSTS_REQUEST,
+    });
+  }, []);
+  
+  useEffect(() => {
+    function onScroll() {
+      if (
+        window.scrollY + document.documentElement.clientHeight >
+        document.documentElement.scrollHeight - 300
+      ) {
+        if (hasMorePost && !loadPostsLoading) {
+          dispatch({
+            type: LOAD_POSTS_REQUEST,
+            data: mainPosts[mainPosts.length - 1].id,
+          });
+        }
+      }
+    }
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [mainPosts, hasMorePost, loadPostsLoading]);
 
   return (
     <IndexLayout>
       <PostForm />
-      <PostCard />
+      {mainPosts.map((c) => (
+        <PostCard key={c.id} post={c} />
+      ))}
     </IndexLayout>
   );
 };
