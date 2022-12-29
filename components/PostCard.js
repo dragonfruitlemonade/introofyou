6476b867eefed5b63214741599bbd1c8abcf8236
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-29 16:16:39 +0900

    feat: 좋아요 토글 기능 구현

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,5 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import Link from 'next';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   FireOutlined,
   FireTwoTone,
@@ -11,27 +13,62 @@ import { Card, Popover, Button, Avatar, List, Comment } from "antd";
 
 import CommentForm from "./CommentForm";
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
+  const dispatch = useDispatch();
+  const [liked, setLiked] = useState(false);
   const [commentFormOpened, setCommentFormOpened] = useState(false);
+  const { removePostLoading } = useSelector((state) => state.post);
+  const { me } = useSelector((state) => state.user);
+  const id = me && me.id;
+
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, []);
 
   const onToggleComment = useCallback(() => {
     setCommentFormOpened((prev) => !prev);
   }, []);
 
+  const onRemovePost = useCallback(() => {
+    dispatch({
+      type: REMOVE_POST_REQUEST,
+      data: post.id,
+    });
+  }, []);
+
   return (
-    <div style={{ marginBottom: 20 }}>
+    <div style={{ marginBottom: 20 }} key={post.id}>
       <Card
         actions={[
-          <FireTwoTone twoToneColor="#cc0000" key="heart" />,
+          liked ? (
+            <FireTwoTone
+              twoToneColor="#cc0000"
+              key="heart"
+              onClick={onToggleLike}
+            />
+          ) : (
+            <FireOutlined key="heart" onClick={onToggleLike} />
+          ),
           <MessageOutlined key="comment" onClick={onToggleComment} />,
           <ArrowDownOutlined />,
           <Popover
             key="more"
             content={
               <Button.Group>
-                <Button>수정</Button>
-                <Button>삭제</Button>
-                <Button>신고</Button>
+                {id && post.UserId === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button
+                      type="danger"
+                      loading={removePostLoading}
+                      onClick={onRemovePost}
+                    >
+                      삭제
+                    </Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
               </Button.Group>
             }
           >
@@ -39,27 +76,46 @@ const PostCard = () => {
           </Popover>,
         ]}
       >
-        <Card.Meta avatar={<Avatar>1</Avatar>} />
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+        />
       </Card>
       {commentFormOpened && (
-        <div>
-          <CommentForm />
-          <List
-            itemLayout="horizontal"
-            renderItem={(item) => (
-              <li>
-                <Comment
-                  author="sehwan"
-                  avatar={<Avatar>S</Avatar>}
-                  content="잘좀하자"
-                />
-              </li>
-            )}
-          />
-        </div>
+          <div>
+            <CommentForm post={post} />
+            <List
+              header={`${post.Comments.length}개의 댓글`}
+              itemLayout="horizontal"
+              dataSource={post.Comments}
+              renderItem={(item) => (
+                <li>
+                  <Comment
+                    author={item.User.nickname}
+                    avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                    content={item.content}
+                  />
+                </li>
+              )}
+            />
+          </div>
       )}
     </div>
   );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.string,
+    Comments: PropTypes.arrayOf(PropTypes.object),
+    Images: PropTypes.arrayOf(PropTypes.object),
+    Likers: PropTypes.arrayOf(PropTypes.object),
+    RetweetId: PropTypes.number,
+    Retweet: PropTypes.objectOf(PropTypes.any),
+  }).isRequired,
 };
 
 
