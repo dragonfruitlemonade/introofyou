28953ae93e47eff2a992ad33d8930ff8de5f9f45
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-30 17:24:52 +0900

    feat: 자기소개서 작성 목록 추가

--- pages/intro.js
+++ pages/intro.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { useCallback } from "react";
-import { Form, Input, Button, Checkbox, Select, Row, Col, DatePicker } from "antd";
+import { Form, Input, Button, Checkbox, Select, Row, Col, DatePicker, Header } from "antd";
+import {
+  Cascader,
+  InputNumber,
+  Radio,
+  Switch,
+  TreeSelect,
+} from "antd";
 
 const { Option } = Select;
 import IndexLayout from "../components/IndexLayout";
@@ -15,7 +22,7 @@ const Intro = () => {
     <IndexLayout>
       <Form style={{ padding: 10 }}>
         <Row>
-          <Col span={11}>
+          {/* <Col span={11}>
             <div>
               <label htmlFor="user-sex">성별</label>
               <hr />
@@ -33,12 +40,93 @@ const Intro = () => {
             <hr />
             <DatePicker onChange={onChange} />
           </Col>
-          <Col span={24}><br/></Col>
           <Col span={24}>
+            <br />
+          </Col> */}
+          <Col span={11}>
+            지원분야
+            <hr />
+            <Input />
+          </Col>
+          <Col span={2}></Col>
+          <Col span={11}>
+            세부전공
+            <hr />
+            <Input />
+          </Col>
+          <Col span={24}>
+            <br />
+          </Col>
+          <Col span={11}>
+            직업
+            <hr />
+            <Input />
+          </Col>
+          <Col span={2}></Col>
+          <Col span={11}>
             이메일
             <hr />
-            tpghks9245@gmail.com
+            <Input />
           </Col>
+          <Col span={24}>
+            <br />
+          </Col>
+          <Col span={11}>
+            희망연봉
+            <hr />
+            <Input />
+          </Col>
+          <Col span={2}></Col>
+          <Col span={11}>
+            포트폴리오
+            <hr />
+            <Input />
+          </Col>
+          <Col span={24}>
+            <br />
+          </Col>
+          <Col span={11}>
+            학력
+            <hr />
+            <Input />
+          </Col>
+          <Col span={13}></Col>
+          <Col span={24}>
+            <br />
+          </Col>
+          <Col span={24}>
+            한줄 자기소개
+            <hr />
+            <Input />
+          </Col>
+          <Col span={24}>
+            <br />
+          </Col>
+          <Col span={24}>
+            skill
+            <hr />
+            <Input />
+          </Col>
+          <Col span={24}>
+            <br />
+          </Col>
+          <Col span={24}>
+            지원동기
+            <hr />
+            <Input.TextArea />
+          </Col>
+          <Col span={24}>
+            <br />
+          </Col>
+          <Col span={24}>
+            자격증 및 수상내역
+            <hr />
+            <Input.TextArea />
+          </Col>
+          <Col span={24}><br /></Col>
+          <Button type="primary" htmlType="submit">
+            저장하기
+          </Button>
         </Row>
       </Form>
     </IndexLayout>
