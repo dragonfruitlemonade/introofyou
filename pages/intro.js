ff3b6e99fc34dd286f72b0d73378f0db110f330b
dragonfruitlemonade <tpghks9245@gmail.com>
2022-12-30 17:25:12 +0900

    feat: 자기소개서 작성 목록 추가

--- pages/intro.js
+++ pages/intro.js
@@ -22,27 +22,6 @@ const Intro = () => {
     <IndexLayout>
       <Form style={{ padding: 10 }}>
         <Row>
-          {/* <Col span={11}>
-            <div>
-              <label htmlFor="user-sex">성별</label>
-              <hr />
-              <Input.Group>
-                <Select defaultValue="선택하기">
-                  <Option>남</Option>
-                  <Option>여</Option>
-                </Select>
-              </Input.Group>
-            </div>
-          </Col>
-          <Col span={2}></Col>
-          <Col span={11}>
-            출생년도
-            <hr />
-            <DatePicker onChange={onChange} />
-          </Col>
-          <Col span={24}>
-            <br />
-          </Col> */}
           <Col span={11}>
             지원분야
             <hr />
