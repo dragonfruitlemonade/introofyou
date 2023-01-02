7f5ef783257d17bf353fee7490af883f5bba8f11
dragonfruitlemonade <tpghks9245@gmail.com>
2023-01-02 13:50:19 +0900

    feat: 자기소개서 작성 내역 label 및 input 옵션 추가

--- pages/intro.js
+++ pages/intro.js
@@ -16,84 +16,139 @@ const Intro = () => {
       <Form style={{ padding: 10 }} onFinish={onFinish}>
         <Row>
           <Col span={11}>
-            지원분야
+            <label htmlFor="user-field">지원분야</label>
             <hr />
-            <Input />
+            <Input
+              name="user-field"
+              value={field}
+              onChange={onChangeField}
+              required
+            />
           </Col>
           <Col span={2}></Col>
           <Col span={11}>
-            세부전공
+            <label htmlFor="user-major">세부전공</label>
             <hr />
-            <Input />
+            <Input
+              name="user-major"
+              value={major}
+              onChange={onChangeMajor}
+              required
+            />
           </Col>
           <Col span={24}>
             <br />
           </Col>
           <Col span={11}>
-            직업
+            <label htmlFor="user-job">직업</label>
             <hr />
-            <Input />
+            <Input
+              name="user-job"
+              value={job}
+              onChange={onChangeJob}
+              required
+            />
           </Col>
           <Col span={2}></Col>
           <Col span={11}>
-            이메일
+            <label htmlFor="user-call">이메일</label>
             <hr />
-            <Input />
+            <Input
+              name="user-call"
+              value={call}
+              onChange={onChangeCall}
+              required
+            />
           </Col>
           <Col span={24}>
             <br />
           </Col>
           <Col span={11}>
-            희망연봉
+            <label htmlFor="user-income">이메일</label>
             <hr />
-            <Input />
+            <Input
+              name="user-income"
+              value={income}
+              onChange={onChangeIncome}
+              required
+            />
           </Col>
           <Col span={2}></Col>
           <Col span={11}>
-            포트폴리오
+            <label htmlFor="user-portfolio">포트폴리오</label>
             <hr />
-            <Input />
+            <Input
+              name="user-portfolio"
+              value={portfolio}
+              onChange={onChangePortfolio}
+              required
+            />
           </Col>
           <Col span={24}>
             <br />
           </Col>
           <Col span={11}>
-            학력
+            <label htmlFor="user-academic">학력</label>
             <hr />
-            <Input />
+            <Input
+              name="user-academic"
+              value={academic}
+              onChange={onChangeAcademic}
+              required
+            />
           </Col>
           <Col span={13}></Col>
           <Col span={24}>
             <br />
           </Col>
           <Col span={24}>
-            한줄 자기소개
+            <label htmlFor="user-sentence">한줄 자기소개</label>
             <hr />
-            <Input />
+            <Input
+              name="user-sentence"
+              value={sentence}
+              onChange={onChangeSentence}
+              required
+            />
           </Col>
           <Col span={24}>
             <br />
           </Col>
           <Col span={24}>
-            skill
+            <label htmlFor="user-skill">skill</label>
             <hr />
-            <Input />
+            <Input
+              name="user-skill"
+              value={skill}
+              onChange={onChangeSkill}
+              required
+            />
           </Col>
           <Col span={24}>
             <br />
           </Col>
           <Col span={24}>
-            지원동기
+            <label htmlFor="user-reason">지원동기</label>
             <hr />
-            <Input.TextArea />
+            <Input.TextArea
+              name="user-reason"
+              value={reason}
+              onChange={onChangeReason}
+              required
+            />
           </Col>
           <Col span={24}>
             <br />
           </Col>
           <Col span={24}>
-            자격증 및 수상내역
+            <label htmlFor="user-other">자격증 및 수상내역</label>
             <hr />
-            <Input.TextArea />
+            <Input.TextArea
+              name="user-other"
+              value={other}
+              onChange={onChangeOther}
+              required
+            />
           </Col>
           <Col span={24}>
             <br />
