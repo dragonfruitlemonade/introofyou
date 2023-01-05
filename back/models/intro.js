ad623cdc5249a318c5fbf57cfec5041c908305e9
dragonfruitlemonade <tpghks9245@gmail.com>
2023-01-05 14:24:50 +0900

    feat: Intro 모델 컬럼 및 로우 작성

---
+++ back/models/intro.js
@@ -0,0 +1,58 @@
+module.exports = (sequelize, DataTypes) => {
+  const Intro = sequelize.define(
+    "Intro",
+    {
+      field: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+      major: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+      job: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+      call: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        unique: true,
+      },
+      income: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+      portfolio: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      academic: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      intro: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      skill: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      reason: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      other: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      charset: "utf8mb4",
+      collate: "utf8mb4_general_ci", // 한글 저장
+    }
+  );
+  Intro.associate = (db) => {};
+  return Intro;
+};
