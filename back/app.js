aa3a50131810f268e798fb088730ce93e60058b0
dragonfruitlemonade <tpghks9245@gmail.com>
2023-01-06 16:20:12 +0900

    feat: 닷엔브기능 추가, 쿠키파서추가, 세션추가, 패스포트 기능 추가

--- back/app.js
+++ back/app.js
@@ -3,9 +3,16 @@ const cors = require('cors');
 // const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const db = require('./models');
+const passport = require('./passport');
 const passportConfig = require('./passport');
+const dotenv = require('dotenv');
+
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
+dotenv.config(); // .env값 치환시켜서 적용.
 db.sequelize
   .sync() // 서버 실행시 디비 연결
   .then(() => {
@@ -13,6 +20,7 @@ db.sequelize
   })
   .catch(console.error);
 
+passportConfig();
 app.use(
   cors({
     origin: 'http://localhost:3000',
@@ -21,6 +29,14 @@ app.use(
 );
 app.use(express.json()); // 아래 코드와 함께 routes에서 req.body를 사용하기 위한 코드, json형식을 req.body안에 넣어줌
 app.use(express.urlencoded({ extended: true })); // form submit을 했을때 데이터를 req.body안에 넣어줌.
+app.use(cookieParser());
+app.use(session({
+  saveUninitialized: false,
+  resave: false,
+  secret: process.env.COOKIE_SECRET,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.send("hello express");
