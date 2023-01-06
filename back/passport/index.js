79ffa1139ac01242a7c9f3d8d7a375e8b614a959
dragonfruitlemonade <tpghks9245@gmail.com>
2023-01-06 16:21:17 +0900

    feat: 아이디만 시리얼화 해주는 기능, 쿠키로 아이디 찾아주는 기능

--- back/passport/index.js
+++ back/passport/index.js
@@ -1,14 +1,22 @@
 const passport = require('passport');
+const { User } = require('../models');
 const local = require('./local');
 
 module.exports = () => {
-  passport.serializeUser(() => {
-
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
   });
 
-  passport.deserializeUser(() => {
-
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({ where: { id } });
+      done(null, user);
+    } catch(error) {
+      console.error(error);
+      done(error);
+    }
+    
   });
 
   local();
-}
+};
