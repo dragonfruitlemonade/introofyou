24e084dfd4e8c6dfb512f113eaab37d53e3115c1
dragonfruitlemonade <tpghks9245@gmail.com>
2023-01-06 13:20:34 +0900

    feat: 로컬 패스포트를 이용한 로그인 코드 작성, 사용자 확인 절차

---
+++ back/passport/local.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const { Strategy: LocalStrategy } = require("passport-local");
+const { User } = require("../models");
+const bcrypt = require("bcrypt");
+
+module.exports = () => {
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+      },
+      async (email, password, done) => {
+        try {
+          const user = await User.findOne({
+            where: { email },
+          });
+          if (!user) {
+            return done(null, false, { reason: "존재하지 않는 사용자입니다." });
+            //첫번째 자리 : 서버에러관련, 두번째 성공여부, 세번째 클라이언트 에러
+          }
+          const result = await bcrypt.compare(password, user.passport);
+          if (result) {
+            return done(null, user); // 두번째 자리가 성공여부를 나타냄. user로 사용자 정보 넘겨줌.
+          }
+          return done(null, false, { reason: "비밀번호가 틀렸습니다." });
+        } catch (error) {
+          console.error(error);
+          return done(error);
+        }
+      }
+    )
+  );
+};
+
+//done -> callback 같은 역할
