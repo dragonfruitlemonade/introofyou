b08148112ecd40e06eebbad9a9a3682e55c301d3
dragonfruitlemonade <tpghks9245@gmail.com>
2023-01-09 23:26:16 +0900

    feat: 포스트 작성하는 라우터 코드

---
+++ back/routes/post.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const { Post } = require('../models');
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+    });
+    res.status(201).json(post);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+})
