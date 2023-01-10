e484d7d01d0a41e0a5ce8d59006c61c85bdc99ab
dragonfruitlemonade <tpghks9245@gmail.com>
2023-01-10 09:53:04 +0900

    feat: 댓글 작성 라우터, 댓글 달 게시글이 존재하는지 확인하는 기능 추가

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
-const { Post } = require('../models');
+const { Post, Comment } = require('../models');
+const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {
+router.post('/', isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.create({
       content: req.body.content,
+      UserId: req.user.id,
     });
     res.status(201).json(post);
   } catch (error) {
@@ -15,3 +17,23 @@ router.post('/', async (req, res, next) => {
     next(error);
   }
 })
+
+router.post("/:postId/comment", isLoggedIn, async (req, res, next) => { // POST /post/1/comment
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.postId }
+    })
+    if (!post) {
+      return res.status(403).send('게시글이 존재하지 않습니다.');
+    }
+    const comment = await Comment.create({
+      content: req.body.content,
+      PostId: req.params.postId,
+      UserId: req.user.id,
+    });
+    res.status(201).json(comment);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
