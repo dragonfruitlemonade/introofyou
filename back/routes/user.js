90682baa1cbe7130bbdfceb93e7d59e0761dd279
dragonfruitlemonade <tpghks9245@gmail.com>
2023-01-10 15:02:06 +0900

    feat: 자기소개서 작성 라우터 코드작성

--- back/routes/user.js
+++ back/routes/user.js
@@ -69,4 +69,28 @@ router.post('/logout', isLoggedIn, (req, res) => {
   res.send('logoutOK');
 });
 
+router.post("/intro", async (req, res, next) => {
+  try {
+    await Intro.create({
+      field: req.body.field,
+      major: req.body.major,
+      job: req.body.job,
+      call: req.body.call,
+      income: req.body.income,
+      portfolio: req.body.portfolio,
+      academic: req.body.academic,
+      intro: req.body.intro,
+      skill: req.body.skill,
+      reason: req.body.reason,
+      other: req.body.other,
+      UserId: req.user.id,
+    });
+    res.status(201).send("ok");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+
 module.exports = router
