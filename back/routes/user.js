3b516241e305a84bff01bf48ea1016e9d3a73ceb
dragonfruitlemonade <tpghks9245@gmail.com>
2023-02-02 11:22:33 +0900

    feat: 닉네임 변경 라우터 작성

--- back/routes/user.js
+++ back/routes/user.js
@@ -121,5 +121,19 @@ router.post("/intro", async (req, res, next) => {
   }
 });
 
+router.patch('./nickname', isLoggedIn, async (req, res, next) => {
+  try {
+    await User.update({
+      nickname: req.body.nickname,
+    }, {
+      where: { id: req.user.id },
+    });
+    res.status(200).json({ nickname: req.body.nickname });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 
 module.exports = router;
